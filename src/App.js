cb3452abdf8956915b614ccf8dd0760c5e88635d
Deep Mandlik <mandlikdeep20@gmail.com>
2023-02-24 00:35:35 +0530

    fix: build issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
 import EnterPattern from "./pages/enterPattern";
 
